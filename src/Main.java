0700bd765a7510109831dfff51da1aa03ff03444
Sgityne <Giovansaraiva@gmail.com>
2025-05-24 02:26:40 -0300

    feat: add menu with validation filter

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import repository.FormRepository;
+import service.MainMenu;
 
 public class Main {
     public static void main(String[] args) {
         FormRepository.createFile();
         FormRepository.readFile();
+        MainMenu.mainMenu();
     }
 }
